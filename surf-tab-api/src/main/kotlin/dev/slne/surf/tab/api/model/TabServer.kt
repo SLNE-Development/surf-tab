94c47b48abf4d0110d17fae5f6e10110de8c5989
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-08-24 00:15:18 +0200

    fix: import tabplayer

--- surf-tab-api/src/main/kotlin/dev/slne/surf/tab/api/model/TabServer.kt
+++ surf-tab-api/src/main/kotlin/dev/slne/surf/tab/api/model/TabServer.kt
@@ -1,8 +1,9 @@
 package dev.slne.surf.tab.api.model
 
+import dev.slne.surf.tab.api.player.TabPlayer
 import it.unimi.dsi.fastutil.objects.ObjectSet
 
 interface TabServer {
-  val name: String
-  fun retrievePlayers(): ObjectSet<TabPlayer>
+    val name: String
+    fun retrievePlayers(): ObjectSet<TabPlayer>
 }
