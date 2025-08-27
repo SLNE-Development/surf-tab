d63bd701bb97d05e86fa402c03fc2ee28501e10f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-08-27 11:13:41 +0200

    refactor: remove unused GameMode conversions and clean up player utility functions

--- surf-tab-velocity/src/main/kotlin/dev/slne/surf/tab/velocity/util/player-util.kt
+++ surf-tab-velocity/src/main/kotlin/dev/slne/surf/tab/velocity/util/player-util.kt
@@ -1,12 +1,10 @@
 package dev.slne.surf.tab.velocity.util
 
-import com.github.retrooper.packetevents.protocol.player.GameMode
 import com.velocitypowered.api.proxy.Player
 import com.velocitypowered.api.util.GameProfile
 import dev.slne.surf.surfapi.core.api.util.toObjectSet
 import dev.slne.surf.tab.api.auth.TabGameProfile
 import dev.slne.surf.tab.api.auth.TabProperty
-import dev.slne.surf.tab.api.player.TabGameMode
 import dev.slne.surf.tab.api.player.TabPlayer
 import dev.slne.surf.tab.core.factory.tabPlayerFactory
 import dev.slne.surf.tab.velocity.plugin
@@ -14,13 +12,6 @@ import kotlin.jvm.optionals.getOrNull
 
 fun Player.tabPlayer() = tabPlayerFactory.createPlayer(this)
 fun TabPlayer.velocityPlayer() = plugin.proxy.getPlayer(this.uniqueId).getOrNull()
-
-fun TabGameMode.toPeGameMode() = when (this) {
-    TabGameMode.SURVIVAL -> GameMode.SURVIVAL
-    TabGameMode.CREATIVE -> GameMode.CREATIVE
-    TabGameMode.ADVENTURE -> GameMode.ADVENTURE
-    TabGameMode.SPECTATOR -> GameMode.SPECTATOR
-}
 
 fun GameProfile.toTabProfile() = TabGameProfile(
     this.id,
