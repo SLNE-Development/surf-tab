73b28b2a39f5020b371c4826c58a328bd83c3c76
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-17 18:35:19 +0100

    feat: reload tablist and format online players in surfTabCommand

--- surf-tab-paper/src/main/kotlin/dev/slne/surf/tab/paper/command/SurfTabCommand.kt
+++ surf-tab-paper/src/main/kotlin/dev/slne/surf/tab/paper/command/SurfTabCommand.kt
@@ -4,7 +4,9 @@ import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.jorel.commandapi.kotlindsl.literalArgument
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import dev.slne.surf.tab.paper.service.tablistService
 import dev.slne.surf.tab.paper.tablistConfiguration
+import org.bukkit.Bukkit
 
 fun surfTabCommand() = commandTree("surftab") {
     withPermission("surf.tab.command.surftab")
@@ -12,6 +14,11 @@ fun surfTabCommand() = commandTree("surftab") {
     literalArgument("reload") {
         anyExecutor { executor, _ ->
             tablistConfiguration.reload()
+
+            Bukkit.getOnlinePlayers().forEach {
+                tablistService.sendAdditions(it)
+                tablistService.formatPlayer(it)
+            }
 
             executor.sendText {
                 appendSuccessPrefix()
