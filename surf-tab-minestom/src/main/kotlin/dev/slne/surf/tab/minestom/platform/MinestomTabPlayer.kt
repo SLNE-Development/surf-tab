a2a3fa6d7344d1aac74adad61110995151364fc5
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-08-21 22:40:55 +0200

    🔧 chore: update version to 1.2.2 and refactor baseName method
    - increment version from 1.2.1 to 1.2.2 in gradle.properties
    - refactor baseName method to include user prefix from LuckPerms

--- surf-tab-minestom/src/main/kotlin/dev/slne/surf/tab/minestom/platform/MinestomTabPlayer.kt
+++ surf-tab-minestom/src/main/kotlin/dev/slne/surf/tab/minestom/platform/MinestomTabPlayer.kt
@@ -1,6 +1,9 @@
 package dev.slne.surf.tab.minestom.platform
 
 import dev.slne.minestom.lobby.api.coroutine.withEntity
+import dev.slne.surf.api.core.luckperms.LuckPermsAccess
+import dev.slne.surf.api.core.luckperms.prefix
+import dev.slne.surf.api.core.minimessage.miniMessage
 import dev.slne.surf.tab.core.client.platform.TabPlayer
 import net.kyori.adventure.text.Component
 import net.minestom.server.entity.Player
@@ -10,7 +13,8 @@ class MinestomTabPlayer(private val player: Player) : TabPlayer {
     override val uuid: UUID get() = player.uuid
     override val audience get() = player
 
-    override fun baseName(): Component = Component.text(player.username)
+    override fun baseName(): Component =
+        miniMessage.deserialize("${LuckPermsAccess.getUser(player.uuid)?.prefix ?: ""}${player.username}")
 
     override suspend fun showTabName(name: Component) {
         player.withEntity {
